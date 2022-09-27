c5e628081d69f7bd30f591b7c72cee93d8c4caac
Alex Kram <alexxc46@gmail.com>
2022-09-27 18:21:37 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import SeasonsDisplay from "./SeasonsDisplay";
+import Spinner from "./Spinner";
+import './App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component {
+    state = { lat: null, errorMessage: '' }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
+        );
+    }
+
+    componentDidUpdate() {
+        console.log('updated')
+    }
+
+    renderContent () {
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if(!this.state.errorMessage && this.state.lat) {
+            return <SeasonsDisplay lat={this.state.lat}/>
+        }
+        return <Spinner/>;
+    }
+
+    render() {
+        return <div className={"border red"}>{this.renderContent()}</div>
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector("#root"));
